e2bb9fc9aa73fe34cf9ccfc36b1d3513a9d88c68
Kerem Kenan Eren <114931693+ErenKerem82@users.noreply.github.com>
2023-05-12 22:56:25 +0300

    super-changes

---
+++ JavaScript/OOP/oop_workspace.js
@@ -0,0 +1,8 @@
+class Employee {
+    constructor(nameSurname, age, job, salary) {
+        this.nameSurname = nameSurname;
+        this.age = age;
+        this.job = job;
+        this.salary = salary;
+    }
+}
