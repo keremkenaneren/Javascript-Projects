b544f29439a718fe6bed444e1eed428af7b62fd7
Kerem Kenan Eren <114931693+ErenKerem82@users.noreply.github.com>
2023-05-12 23:11:37 +0300

    oop-commit

--- JavaScript/OOP/oop_workspace.js
+++ JavaScript/OOP/oop_workspace.js
@@ -5,4 +5,18 @@ class Employee {
         this.job = job;
         this.salary = salary;
     }
+
+    showInfo() {
+        console.log(
+            `
+            Name-Surname : ${this.nameSurname}
+            Age : ${this.age}
+            Job : ${this.job}
+            Salary : ${this.salary}
+            `
+        );
+    }
 }
+
+const employe1 = new Employee("Kerem Kenan", 16, "Full Stack Web Developer", 15000);
+employe1.showInfo();
